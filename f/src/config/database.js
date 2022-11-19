fb0fc5ebacbe0a089c09f5fb1590f26896f9d2d5
susan0jone <67920116+susan0jone@users.noreply.github.com>
2022-11-19 01:03:19 +0100

    edit

---
+++ f/src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+const{config}=require("dotenv")
+config()
+async function connect(uri) {
+    try{
+    mongoose.connect(uri || process.env.MONGO_DB_LOCAL)
+    console.log("connected to mongodb")
+    }catch(error){
+        console.log(error.message)
+    }
+}
+module.exports= connect
