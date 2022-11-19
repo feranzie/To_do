303f9838198e0437858542327022a5d922738f69
Feranmi Ola-davies <oladaviesferanmi@gmail.com>
2022-11-19 01:25:35 +0100

    Delete f directory

--- f/src/router/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-const router=require("express").Router();
-const controller=require("../controller/taskController");
-
-router
-.get("/", controller.getAllTasks)
-.post("/", controller.addTask)
-.put("/", controller.updateTask)
-.delete("/:id", controller.deleteTask);
-module.exports=router;
